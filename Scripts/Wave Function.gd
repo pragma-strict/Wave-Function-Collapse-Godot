95218d0765037f7bae2da17b6dd85332951c69d1
Ian <ianrdejong@gmail.com>
2021-03-05 01:37:36 -0800

    Refactor. Remove multidimensional arrays. Much clean

--- Scripts/Wave Function.gd
+++ Scripts/Wave Function.gd
@@ -6,88 +6,97 @@ extends Spatial
 var prototypes = Prototypes.new()
 var rng = RandomNumberGenerator.new()
 
-export var function_width = 4
+export var field_width = 16
+export var field_height = 4
 export var cell_size = 2
-var cell_superpositions = []		# 3D matrix where each element is a list of indexes into prototypes.list array
-var cell_nodes = []		# Init with empty spatial nodes
-var num_cells = pow(function_width, 3)
+var cell_nodes = []		# Node refs
+var cell_superpositions = [] # For each cell, a list of indexes into prototypes.list array
+var num_cells = field_width * field_width * field_height
+
+var stride_x
+var stride_y
+var stride_z
 
 
-func regenerate_mesh_for_cell(x_index, y_index, z_index):
-	var superp_tmp = cell_superpositions[x_index][y_index][z_index]
+# Create stuff
+func _init():
+	rng.randomize()
+	stride_x = field_width * field_height
+	stride_y = field_height
+	stride_z = 1
+	
+	for i in range(num_cells):
+		cell_superpositions.append(prototypes.get_max_entropy_index_list())
+		var temp_node = Spatial.new()
+		add_child(temp_node)
+		cell_nodes.append(temp_node)
+		regenerate_mesh_for_cell(i)
+
+
+# Call debug functions on input
+func _input(event):
+	if (event is InputEventKey and event.pressed):
+		if (event.scancode == KEY_SPACE):
+			
+			# Collapse a test cell
+			var index = rng.randi_range(0, len(cell_nodes) -1)
+			superpos_collapse_to_random(index)
+			regenerate_mesh_for_cell(index)
+			print(cell_superpositions[index])
+
+		if (event.scancode == KEY_G): # Test function key
+			pass
+
+
+# Use cell superpos to load reload its mesh with the most up-to-date version
+func regenerate_mesh_for_cell(index:int):
+	var cell_coordinates = cell_index_to_coordinate(index)
+	var cell_superpos = cell_superpositions[index]
 	var new_mesh = MeshInstance.new()
 	
-	if (len(superp_tmp) == 1):
-		new_mesh.mesh = load(prototypes.proto_list[superp_tmp[0]]["mesh_ref"])
+	if (len(cell_superpos) == 1):
+		new_mesh.mesh = load(prototypes.proto_list[cell_superpos[0]]["mesh_ref"])
 	else:
 		new_mesh.mesh = load("res://Meshes/chunk_null.obj")
-		
-	new_mesh.translation.x = x_index * cell_size
-	new_mesh.translation.y = y_index * cell_size
-	new_mesh.translation.z = z_index * cell_size
-	cell_nodes[x_index][y_index][z_index].free() 	# Delete old node
-	cell_nodes[x_index][y_index][z_index] = new_mesh	# Log new node
+	
+	new_mesh.translation.x = cell_coordinates.x * cell_size
+	new_mesh.translation.y = cell_coordinates.y * cell_size
+	new_mesh.translation.z = cell_coordinates.z * cell_size
+	cell_nodes[index].free() 	# Delete old node
+	cell_nodes[index] = new_mesh	# Log new node
 	add_child(new_mesh)	# Add new node to scene
 
 
-func _init():
-	for x in range(function_width):
-		cell_superpositions.append([])
-		cell_nodes.append([])
-		for y in range(function_width):
-			cell_superpositions[x].append([])
-			cell_nodes[x].append([])
-			for z in range(function_width):
-				cell_superpositions[x][y].append(prototypes.get_max_entropy_index_list())
-				var temp_node = Spatial.new()
-				add_child(temp_node)
-				cell_nodes[x][y].append(temp_node)
-				regenerate_mesh_for_cell(x, y, z)
-
-
 # Removes the illegal protos from cell superposition. Assumes valid index args.
-func superpos_restrict_single(x_index, y_index, z_index, illegal_protos:Array):
-	var superp_tmp = cell_superpositions[x_index][y_index][z_index]
+func superpos_restrict_single(index:int, illegal_protos:Array):
+	var superp_tmp = cell_superpositions[index]
 	for restriction in illegal_protos:
 		if superp_tmp.has(restriction):
 			superp_tmp.remove(superp_tmp.find(restriction))
-	cell_superpositions[x_index][y_index][z_index] = superp_tmp # This line might not be needed if superp_tmp is ref
+	cell_superpositions[index] = superp_tmp # This line might not be needed if superp_tmp is ref
 
 
 # Removes all but one proto index from superposition, collapsing the cell to given state 
-func superpos_collapse_to_value(x_index, y_index, z_index, value):
-	cell_superpositions[x_index][y_index][z_index] = value
+func superpos_collapse_to_value(index:int, value):
+	cell_superpositions[index] = value
 
 
 # Removes all but one proto index from superposition, collapsing the cell to random possible state 
-func superpos_collapse_to_random(x_index, y_index, z_index):
-	var collapsed_proto = rng.randi_range(0, len(cell_superpositions[x_index][y_index][z_index]) -1)
-	superpos_restrict_recursive(x_index, y_index, z_index, [collapsed_proto])
-	
-	# Collapse a neighboring cell to see if we have compatibility errors
-	if (x_index < 3):
-		var possible_neighbors = prototypes.get_compatible_protos(collapsed_proto, 'xP')
-		var random_selection = rng.randi_range(0, len(possible_neighbors) -1)
-		var neighbor_proto_index = possible_neighbors[random_selection]
-		superpos_restrict_recursive(x_index+1, y_index, z_index, [neighbor_proto_index])
-		regenerate_mesh_for_cell(x_index+1, y_index, z_index)
-	else:
-		var possible_neighbors = prototypes.get_compatible_protos(collapsed_proto, 'xN')
-		var random_selection = rng.randi_range(0, len(possible_neighbors) -1)
-		var neighbor_proto_index = possible_neighbors[random_selection]
-		superpos_restrict_recursive(x_index-1, y_index, z_index, [neighbor_proto_index])
-		regenerate_mesh_for_cell(x_index-1, y_index, z_index)
-	
-	
-func superpos_restrict_recursive(x_index, y_index, z_index, new_superpos:Array):
-	cell_superpositions[x_index][y_index][z_index] = new_superpos # Update superpos for this cell
+func superpos_collapse_to_random(index:int):
+	var rand_proto_decision = rng.randi_range(0, len(cell_superpositions[index]) -1)
+	cell_superpositions[index] = [rand_proto_decision]
+
+
+# I don't remember what this was originally supposed to do. Now it does nothing.
+func superpos_restrict_recursive(index:int, new_superpos:Array):
+	cell_superpositions[index] = new_superpos # Update superpos for this cell
 	for proto_index in new_superpos:
 		var xP_compatibility_dict = {}
 		#var xP_compatible_protos = prototypes.get_compatible_protos(proto_index, 'xP')
-		print("xP compatible: ", prototypes.get_compatible_protos(proto_index, 'xP'))
-		print("xN compatible: ", prototypes.get_compatible_protos(proto_index, 'xN'))
-		print("yP compatible: ", prototypes.get_compatible_protos(proto_index, 'yP'))
-		print("yN compatible: ", prototypes.get_compatible_protos(proto_index, 'yN'))
+		#print("xP compatible: ", prototypes.get_compatible_protos(proto_index, 'xP'))
+		#print("xN compatible: ", prototypes.get_compatible_protos(proto_index, 'xN'))
+		#print("yP compatible: ", prototypes.get_compatible_protos(proto_index, 'yP'))
+		#print("yN compatible: ", prototypes.get_compatible_protos(proto_index, 'yN'))
 	# To begin, I know all the possible states of this node (my superpos)
 	# For each adjacent node I want to find the set of restrictions on that node
 	# I can do that by tallying all the possible sockets that that node would have to have on its border with this node
@@ -95,20 +104,62 @@ func superpos_restrict_recursive(x_index, y_index, z_index, new_superpos:Array):
 	# Any nodes with those sockets will be a restriction on next node
 	# If there is a restriction I'll make a list of the possible states of the next node and recursively call
 	# If there are no restrictions I'll simply return	
+	pass
 
 
-func _input(event):
-	if (event is InputEventKey and event.pressed):
-		if (event.scancode == KEY_SPACE):
-			
-			# Collapse a test cell
-			rng.randomize()
-			var row = rng.randi_range(0, function_width - 1)
-			var col = rng.randi_range(0, function_width - 1)
-			var depth = rng.randi_range(0, function_width - 1)
-			superpos_collapse_to_random(row, col, depth)
-			regenerate_mesh_for_cell(row, col, depth)
-			print(cell_superpositions[row][col][depth])
+# Return the index adjacent to origin in the given direction
+func get_index_adjacent_to(origin:int, direction:Vector3):
+	var coordinate = cell_index_to_coordinate(origin)
+	
+	if(direction == Vector3.UP):
+		if(coordinate.y < field_height -1):
+			return origin + stride_z
+		else:
+			return cell_coordinate_to_index(Vector3(coordinate.x, 0, coordinate.z))
+	
+	if(direction == Vector3.DOWN):
+		if(coordinate.y > 0):
+			return origin - stride_z
+		else:
+			return cell_coordinate_to_index(Vector3(coordinate.x, field_height -1, coordinate.z))
+	
+	if(direction == Vector3.RIGHT):
+		if(coordinate.x < field_width -1):
+			return origin + stride_x
+		else:
+			return cell_coordinate_to_index(Vector3(0, coordinate.y, coordinate.z))
+	
+	if(direction == Vector3.LEFT):
+		if(coordinate.x > 0):
+			return origin - stride_x
+		else:
+			return cell_coordinate_to_index(Vector3(field_width -1, coordinate.y, coordinate.z))
+	
+	if(direction == Vector3.FORWARD):
+		if(coordinate.z > 0):
+			return origin - stride_z
+		else:
+			return cell_coordinate_to_index(Vector3(coordinate.x, coordinate.y, field_width -1))
+	
+	if(direction == Vector3.BACK):
+		if(coordinate.z < field_width -1):
+			return origin + stride_z
+		else:
+			return cell_coordinate_to_index(Vector3(coordinate.x, coordinate.y, 0))
 
-		if (event.scancode == KEY_G): # Test function key
-			pass
+
+# Return the Vector3 coordinate of the cell at a given index
+func cell_index_to_coordinate(index:int):
+	var coordinate = Vector3()
+	coordinate.x = floor(index / (field_width * field_height))
+	coordinate.y = int(floor(index / field_width)) % field_height
+	coordinate.z = index % field_width
+	return coordinate
+
+
+# Return the index of the cell at a given coordinate
+func cell_coordinate_to_index(coordinate:Vector3):
+	var index = coordinate.x * stride_x
+	index += coordinate.y * stride_y
+	index += coordinate.z * stride_z
+	return index
